009ddde53a107a5fd54c2da9a4b5d3730d747b05
ellalearns <ellalearnscopy@gmail.com>
2024-03-22 18:31:59 +0100

    create base model class and test

---
+++ models/base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this is the base model module
+here the base model is defined
+and needed attributes and methods
+"""
+
+
+class BaseModel():
+    pass
---
+++ tests/test_base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+unittests for the base model class
+"""
+
+
+import unittest
+
+
+class TestBaseModel():
+    pass
