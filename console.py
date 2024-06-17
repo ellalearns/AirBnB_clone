67e4e5d22ba9f21f56d3220faa614676bbdfe681
ellalearns <ellalearnscopy@gmail.com>
2024-06-17 21:22:14 +0100

    code do_create

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ main entry point for console tool
 
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -38,6 +39,23 @@ class HBNBCommand(cmd.Cmd):
         help about quit function
         """
         print("wanna quit?")
+    
+    def do_create(self, command):
+        """
+        creates a new instance of BaseModel
+        saves it to json file
+        prints the id
+        """
+        if not command:
+            print("*** class name missing ***")
+            return None
+        command = command.split()
+        if command[0] == "BaseModel":
+            newBaseModel = BaseModel()
+            newBaseModel.save()
+            print(newBaseModel.id)
+        else:
+            print("*** class doesn't exist ***")
 
 
 if __name__ == '__main__':
